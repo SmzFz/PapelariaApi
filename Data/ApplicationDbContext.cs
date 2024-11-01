93872c5c781c1aa3371110733d848c46fcc3b3cc
Felipe Salmazo <felipesalmazo123@gmail.com>
2024-11-01 16:46:35 -0300

    Back-End finalizado

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PapelariaAPI.Models;
+
+namespace PapelariaAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<PapelariaAPI.Models.Cliente> Cliente { get; set; } = default!;
+        public DbSet<PapelariaAPI.Models.Fornecedor> Fornecedor { get; set; } = default!;
+        public DbSet<PapelariaAPI.Models.Produto> Produto { get; set; } = default!;
+        public DbSet<PapelariaAPI.Models.Venda> Venda { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,23 @@
+using PapelariaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Adicionando conexão com o banco de dados em SQL Server
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionSenai")));
+
+//permitindo acesso externo a API, utlizando CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,5 +38,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowAll");
 
 app.Run();
